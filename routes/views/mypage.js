6a4d9e5323cbc51c9e8333e9377f613ff6d595d2
byul5810 <dkim5@g.emporia.edu>
2019-02-23 07:37:37 -0600

    Create mypage and edit sign up page

--- routes/views/mypage.js
+++ routes/views/mypage.js
@@ -1,5 +1,7 @@
 var keystone = require('keystone');
 const firebase = require('firebase');
+var Customer = keystone.list('Customer');
+
 exports = module.exports = function (req, res) {
 
     var view = new keystone.View(req, res);
@@ -10,6 +12,32 @@ exports = module.exports = function (req, res) {
     // locals.section is used to set the currently selected
     // item in the header navigation.
     locals.section = 'mypage';
+
+    view.on('init', function (next) {
+        const user = firebase.auth().currentUser
+        if (user) {
+            console.log("[routes/main.js] Welcome Customer! you're currently logged in");
+            locals.user = user
+            const uuid = user.uid
+
+            Customer.model.findOne({ uid: uuid }).exec(function (err, data) {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send('DB Error');
+                    next();
+                }
+                locals.customer = data;
+                console.log(data);
+                next();
+            });
+
+        } else {
+            console.log("[routes/main.js] You're not logged in");
+            next()
+        }
+    })
+
+
 
     view.render('mypage', { layout: 'main' })
 };
